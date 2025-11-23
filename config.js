7046f59ea51cea2f2f300a28f8300c1919a9b7bc
enzoisgod <143012547+enzoisgod@users.noreply.github.com>
2025-11-23 23:07:31 +0100

    Fix formatting in config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 const config = {
-  token: process.env.TOKEN,
-  clientId: process.env.CLIENT_ID,
-  guildId: process.env.GUILD_ID,
-  logChannelId: process.env.LOG_CHANNEL_ID,
-  guildRoleId: process.env.GUILD_ROLE_ID,
-  ownerIds: process.env.OWNER_IDS ? process.env.OWNER_IDS.split(',') : []
+    token: process.env.TOKEN,
+    clientId: process.env.CLIENT_ID,
+    guildId: process.env.GUILD_ID,
+    logChannelId: process.env.LOG_CHANNEL_ID,
+    guildRoleId: process.env.GUILD_ROLE_ID,
+    ownerIds: process.env.OWNER_IDS ? process.env.OWNER_IDS.split(',') : []
 };
 
 module.exports = config;
